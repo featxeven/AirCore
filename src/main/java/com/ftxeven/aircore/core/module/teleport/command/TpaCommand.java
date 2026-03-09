d63112cbed7b597cde8cc49dbb2016a7a9ae5dd8
Sebastian <2003.sebrod@gmail.com>
2026-03-08 20:23:30 -0500

    fixed message key

--- src/main/java/com/ftxeven/aircore/core/module/teleport/command/TpaCommand.java
+++ src/main/java/com/ftxeven/aircore/core/module/teleport/command/TpaCommand.java
@@ -102,7 +102,7 @@ public final class TpaCommand implements TabExecutor {
         MessageUtil.send(target, "teleport.requests.tpa-from", Map.of("player", player.getName()));
 
         if (plugin.utility().afk().isAfk(target.getUniqueId())) {
-            MessageUtil.send(player, "errors.afk-interaction-notify", Map.of("player", target.getName()));
+            MessageUtil.send(player, "errors.afk.interaction-notify", Map.of("player", target.getName()));
         }
 
         return true;
